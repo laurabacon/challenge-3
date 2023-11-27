989c2392008b911d0965a6735675bf76af61943e
Laura Brown <laurabrown@Lauras-MacBook-Pro.local>
2023-11-27 09:51:04 -0700

    entire array

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 // Assignment code here
 
 var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
-var uppercharacters = ["A", "B", "C", "D", "E", "F", "G"];
-var lowerCharacters = ["a", "b", "c"];
-var specialcharacters = ["!", "@", "#"];
+var uppercharacters = ["A", "B", "C", "D", "E", "F", "G", "H", "I, "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var lowerCharacters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var specialcharacters = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")"];
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -61,17 +61,3 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
-// WHEN I click the button to generate a password. THEN I am presented with a series of prompts for password criteria
-
-//WHEN prompted for password criteria. THEN I select which criteria to include in the password
-
-//WHEN prompted for the length of the password. THEN I choose a length of at least 8 characters and no more than 128 characters
-
-//WHEN asked for character types to include in the password. THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
-
-//WHEN I answer each prompt. THEN my input should be validated and at least one character type should be selected
-
-//WHEN all prompts are answered. THEN a password is generated that matches the selected criteria
-
-//WHEN the password is generated. THEN the password is either displayed in an alert or written to the page
